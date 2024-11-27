40662cc218d0a416600ddf1149822f367d7cebd6
DAKSH THAKUR <70480346+Just-DT21@users.noreply.github.com>
2024-11-28 03:14:37 +0530

    Update lab_4_que-2.c

--- lab_4_que-2.c
+++ lab_4_que-2.c
@@ -3,6 +3,9 @@ int main() {
     int year;
     printf("Enter a year: ");
     scanf("%d", &year);
-    printf("%s\n", (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) ? "Leap Year" : "Not Leap Year");
+    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+        printf("Leap Year\n");
+    else
+        printf("Not Leap Year\n");
     return 0;
 }
