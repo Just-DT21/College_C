ebdd4f26d699ebd4824e1c381493c136ad13dec0
DAKSH THAKUR <70480346+Just-DT21@users.noreply.github.com>
2024-12-02 14:20:32 +0530

    Update lab_8_que-5.c

--- lab_8_que-5.c
+++ lab_8_que-5.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 int main() {
-    int arr[] = {4, 7, 1, 9, -3, 2}, n = 6, rev[6];
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    int arr[n], rev[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+
     for (int i = 0; i < n; i++) rev[i] = arr[n - i - 1];
+    printf("Reversed array: ");
     for (int i = 0; i < n; i++) printf("%d ", rev[i]);
     return 0;
 }
