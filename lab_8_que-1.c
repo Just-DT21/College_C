7ef8db0adb943c9d3170a37fd872fc64afe81b28
DAKSH THAKUR <70480346+Just-DT21@users.noreply.github.com>
2024-12-02 14:19:21 +0530

    Update lab_8_que-1.c

--- lab_8_que-1.c
+++ lab_8_que-1.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 int main() {
-    int arr[] = {4, 7, 1, 9, -3, 2}, n = 6, max = arr[0], min = arr[0];
+    int n, max, min;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+    max = min = arr[0];
     for (int i = 1; i < n; i++) {
         if (arr[i] > max) max = arr[i];
         if (arr[i] < min) min = arr[i];
