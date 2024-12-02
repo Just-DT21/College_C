9d38e261534a2ddc405358b01dcedf19ddc23892
DAKSH THAKUR <70480346+Just-DT21@users.noreply.github.com>
2024-12-02 14:19:54 +0530

    Update lab_8_que-3.c

--- lab_8_que-3.c
+++ lab_8_que-3.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 int main() {
-    int arr[] = {4, 7, 1, 9, -3, 2}, l = 1, r = 4;
+    int n, l, r;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+    printf("Enter start and end indices: ");
+    scanf("%d %d", &l, &r);
+
+    printf("Subarray: ");
     for (int i = l; i <= r; i++) printf("%d ", arr[i]);
     return 0;
 }
